0507fa2c7dabdfbaf5c495018eea868c379afe70
John Brown <100054328@csisd.org>
2025-01-16 14:55:20 -0600

    visuals added

--- MyPanel.java
+++ MyPanel.java
@@ -13,9 +13,14 @@ int sortType;
 boolean sorting;
 int selected;
 boolean step;
+boolean done;
+int countdown;
 
 int index;
 int secondIndex;
+
+int barWidth;
+int barHeight;
  
   MyPanel(int arrLength, int sort) {
     arr = new int[arrLength];
@@ -25,43 +30,73 @@ int secondIndex;
     mixUp();
     System.out.println("Starting Array: " + Arrays.toString(arr));
 
-    timer = new Timer(5, this);
+    timer = new Timer(2, this);
 	  timer.start();
+    countdown = 200;
 
     sortType = sort;
     sorting = false;
+    done = false;
 
     index = 0;
     secondIndex = 0;
 
     this.setPreferredSize(new Dimension(1920,1080));
     this.setBackground(Color.white);
+
+    //figuring out dimensions
+    barHeight = (960 / arrLength);
+    barWidth = (1800 / arrLength);
+
   }
 
   public void paint(Graphics g) {
     Graphics2D g2D = (Graphics2D) g;
+    g2D.clearRect(0, 0, 1920, 1080);
+
+    g2D.setColor(Color.black);
+    g2D.fillRect(50, 980, 1800, 5);
+
+    g2D.fillRect(50, 20 + arr[0] * barHeight, barWidth, 1000);
+
+    for (int x = 0; x < arr.length; x++) {
+      if (x == index) {
+        g2D.setColor(Color.orange);
+      } else {
+        g2D.setColor(Color.black);
+      }
+     g2D.fillRect(50 + x * barWidth, 20 + arr[x] * barHeight, barWidth, 1000);
+    }
     
   }
   //NEW FRAME
   @Override
 	public void actionPerformed(ActionEvent e) {
-    if (checkSort()) {
-      System.out.println("Good job! Ya did it!");
-    } else {
-      if (sortType == 1) {//Bubble
-        bubSort();
+    if (countdown == 0) {
+      if (checkSort()) {
+        if (!done) {
+          System.out.println("Good job! Ya did it!");
+          done = true;
+        }
+      } else {
+        if (sortType == 1) {//Bubble
+          bubSort();
+        }
+        System.out.println(Arrays.toString(arr));
+        repaint();
       }
-      System.out.println(Arrays.toString(arr));
+    } else {
+      countdown--;
     }
   }
   private void bubSort() {
-    if (index > arr.length - 1) {
+    if (index >= arr.length - 1) {
       index = 0;
     }
     if (arr[index] > arr[index + 1]) {
       swap(index, index + 1);
     }
-    ++index;
+    index++;
   }
   private boolean checkSort() {
     boolean sorted = true;
